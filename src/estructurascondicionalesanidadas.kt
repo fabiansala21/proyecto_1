56fdb5b165f3ca27edb098224871fbeed2945399
fabiansala <fabiansala0821@gmail.com>
2024-06-03 13:55:52 -0400

    estructurascondicionalesanidadas

---
+++ src/estructurascondicionalesanidadas.kt
@@ -0,0 +1,80 @@
+fun main () {
+/*
+    print("ingrese la primera nota")
+    val nota1: Int = readln().toInt()
+    print("ingrese la segunda nota")
+    val nota2: Int = readln().toInt()
+    print("ingrese la tercera nota")
+    val nota3: Int = readln().toInt()
+
+    val promedio = (nota1 + nota2 +nota3) / 3
+    if (promedio >= 7)
+        print("promocionado")
+    else
+        if (promedio >= 4)
+            print("regular")
+    else
+        print("reprobado")
+    */
+
+/*
+    print("ingrese la primera nota")
+    val nota1: Int = readln().toInt()
+    print("ingrese la segunda nota")
+    val nota2: Int = readln().toInt()
+    print("ingrese la tercera nota")
+    val nota3: Int = readln().toInt()
+
+    val promedio = (nota1 + nota2 +nota3) / 3
+
+    val estado = if (promedio >=7)"promocionado"
+    else if (promedio >=4)"regular" else "reprobado"
+
+    print("estado del alumno $estado")
+        */
+
+    //problema1
+
+    print("ingrese el primer valor")
+    val valor1 = readln().toInt()
+    print("ingrese el segundo valor:")
+    val valor2 = readln().toInt()
+    print("ingrese el terser valor:")
+    val valor3 = readln().toInt()
+
+    val mayor = if (valor1 > valor2) valor1 else valor2
+
+    println("el mayor $valor1 y $valor2 es $mayor")
+    
+
+    //problema3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
